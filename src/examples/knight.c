8c65dda4a9e442837a0ac6e673631e6ef400615d
foxik384 <alexx384@yandex.ru>
2016-11-19 22:20:35 +0300

    All done. I also created program knight

--- src/examples/knight.c
+++ src/examples/knight.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
+//#include "../threads/malloc.h"
+
+int max_x, max_y;
 
 int dx[8] = { -2, -1, 1, 2, 2, 1, -1, -2 };
 int dy[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-int desk[8][8] = { NULL };
+int desk[10][10]={NULL};
 int min;
 int mx, my;
 int counter = 1;
 
 void search_move(int x, int y);
 void move(int k, int x, int y);
-void desk_init();
+//void desk_init();
 void show_desk();
 
 
 void search_move (int x, int y){
-  if (x > 7 || y > 7 || x < 0 || y < 0 || desk[y][x] > 0)
+  if (x > max_x || y > max_y || x < 0 || y < 0 || desk[y][x] > 0)
     return;
   int local_min = 0;
   for (int i = 0; i < 8; i++) {
-    if (!(x + dx[i] > 7 || y + dy[i] > 7 || x + dx[i] < 0 || y + dy[i] < 0 || desk[y + dy[i]][x + dx[i]] > 0))
+    if (!(x + dx[i] > max_x || y + dy[i] > max_y || x + dx[i] < 0 || y + dy[i] < 0 || desk[y + dy[i]][x + dx[i]] > 0))
       local_min++;
   }
   if (local_min < min) {
@@ -40,24 +42,14 @@ void move(int k, int x, int y) {
   }
   desk[my][mx] = counter;
   counter++;
-  //system("cls");
-  //show_desk();
-  //system("pause");
+  
   move(k - 1, mx, my);
-}
-
-void desk_init (){
-  int i, j;
-  for (i = 0; i < 8; i++)
-    for (j = 0; j < 8; j++)
-      desk[i][j] = 0;
-  return;
 }
 
 void show_desk() {
   int i, j;
-  for (i = 0; i < 8; i++) {
-    for (j = 0; j < 8; j++) {
+  for (i = 0; i < max_y+1; i++) {
+    for (j = 0; j < max_x+1; j++) {
       if (desk[i][j] < 10)
         printf("0%d ", desk[i][j]);
       else
@@ -67,15 +59,26 @@ void show_desk() {
   }
 }
 
-void main(int argc, char *argv[]) {
-  desk_init();
+int main(int argc, char *argv[])
+{
+//  desk_init();
   mx=atoi(argv[1]);
   my=atoi(argv[2]);
+  max_x=atoi(argv[3])-1;
+  max_y=atoi(argv[4])-1;
 //  printf("Print y: ");
 
   int buffx = mx;
   int buffy = my;
-  int n = 0;
+  int n = 0, i;
+
+  //desk=calloc(max_y+1, 4);
+  //for (i = 0; i < max_y+1; ++i)
+  //{
+  //  desk[i]=calloc(max_x+1,4);
+  //}
+
+
   desk[my][mx] = counter;
   counter++;
   //system("cls");
@@ -84,7 +87,7 @@ void main(int argc, char *argv[]) {
     move(63, mx, my);
     mx = buffx;
     my = buffy;
-    desk_init;
+    
     counter = 1;
     desk[my][mx] = counter;
     counter++;
@@ -93,6 +96,12 @@ void main(int argc, char *argv[]) {
   printf("\n");
   show_desk();
   printf("\n");
+
+  //for (i = 0; i < max_y+1; ++i)
+  //{
+  //  free(desk[i]);
+ // }
+  //free(desk);
 
   return 0;
 }
