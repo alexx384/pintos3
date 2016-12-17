33009d557eb8a7523cd5c78e6c391dcdd573a9c3
foxik384 <alexx384@yandex.ru>
2016-12-17 21:03:51 +0300

    Added new tests

--- src/userprog/test.sh
+++ src/userprog/test.sh
@@ -4,4 +4,5 @@ rm -vf filesys.dsk
 pintos-mkdisk filesys.dsk --filesys-size=2
 pintos --qemu --disk=filesys.dsk -- -f -q
 pintos --qemu --disk=filesys.dsk -p ../../examples/memstat -a memstat -- -q
+pintos --qemu --disk=filesys.dsk -p ../../examples/fsinfo -a fsinfo -- -q
 pintos --qemu --disk=filesys.dsk -- -q run 'memstat'
